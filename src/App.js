1271085845d9224a68c0937a3ef29b80388a6de9
larrytu327 <larrytu327@gmail.com>
2023-08-15 22:52:15 -0700

    changed formatting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import { useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
