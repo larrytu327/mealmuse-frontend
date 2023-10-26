ef1a3b70150add166769ec6a7071f299c0abd5a0
larrytu327 <larrytu327@gmail.com>
2023-10-25 21:43:28 -0700

    added remove button to list of restaurants for randomizer

--- src/pages/myRestaurants.js
+++ src/pages/myRestaurants.js
@@ -79,6 +79,16 @@ const MyRestaurants = ({isLoggedIn, token}) => {
         }
     };
 
+    const removeFromRandomizer = async (restaurant) => {
+        try {
+            const index = user.addedToRandomizer.indexOf(restaurant);
+            console.log(index);
+            
+        } catch(err) {
+            console.log(err);
+        }
+    };
+
     const randomizer = async (array) => {
         try {
             const random = Math.floor(Math.random()*(array.length))+1;
@@ -128,6 +138,7 @@ const MyRestaurants = ({isLoggedIn, token}) => {
                         <>
                             <p>
                                 <Link key={restaurant._id} className='h4' to={`/restaurants/${restaurant._id}`}>{restaurant.name}</Link>
+                                <button class="btn btn-danger" onClick={() => {removeFromRandomizer(restaurant)}}>Remove</button>
                             </p>
                         </>
                     ))
